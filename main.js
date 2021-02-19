d96fdfa05245c300e3b85b0fc7a1315a9f983732
samantha <lattechoi2@gmail.com>
2021-02-19 18:04:59 +0900

    JS, CSS 성능 개선 코드

--- main.js
+++ main.js
@@ -3,16 +3,21 @@ const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
-document.addEventListener('mousemove', (event) => {
-    const x = event.clientX;
-    const y = event.clientY;
+addEventListener('load', () => {
+    const targetRect = target.getBoundingClientRect();
+    const targetHalfWidth = targetRect.width / 2;
+    const targetHalfHeight = targetRect.height / 2;
 
-    vertical.style.left = `${x}px`;
-    horizontal.style.top = `${y}px`;
-    target.style.top = `${y}px`;
-    target.style.left = `${x}px`;
-    tag.style.top = `${y}px`;
-    tag.style.left = `${x}px`;
+    document.addEventListener('mousemove', (event) => {
+        const x = event.clientX;
+        const y = event.clientY;
+
+        vertical.style.transform = `translateX(${x}px)`;
+        horizontal.style.transform = `translateY(${y}px)`;
+
+        target.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;
+        tag.style.transform = `translate(${x + 25}px, ${y + 20}px)`;
     
-    tag.innerHTML = `${x}px, ${y}px`;
-});
+        tag.innerHTML = `${x}px, ${y}px`;
+    });
+})
