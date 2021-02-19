21bf39666dcd57f1d1ddb3e22df9a5a9e93a2896
samantha <lattechoi2@gmail.com>
2021-02-19 17:34:39 +0900

    클래스 이름 오타 수정

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 const vertical = document.querySelector('.vertical');
-const horozontal = document.querySelector('.horozontal');
+const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
 document.addEventListener('mousemove', (event) => {
     const x = event.clientX;
     const y = event.clientY;
-    console.log(`${x} ${y}`);
 
     vertical.style.left = `${x}px`;
-    horozontal.style.top = `${y}px`;
+    horizontal.style.top = `${y}px`;
     target.style.top = `${y}px`;
     target.style.left = `${x}px`;
     tag.style.top = `${y}px`;
